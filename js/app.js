bb4094a5aea0f383023260581bdfce726c1de41f
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-02-05 12:10:01 -0500

    [Update] - Create object according to the filling of the selectors and filter by item brand.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,30 @@
 //Variables.
-const resultado = document.querySelector('#resultado');
+
+//Acceso a selectores de parametros.
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+//Contenedor de resultados.
+const resultado = document.querySelector('#resultado');
+
+
 const max = new Date().getFullYear();
 const min = max - 10;
+
+//Crear un objeto con la busqueda.
+const datosBusqueda = {
+  marca: '',
+  year: '',
+  minimo: '',
+  maximo: '',
+  puertas: '',
+  transmision: '',
+  color: '',
+}
 
 // Eventos.
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -14,6 +36,32 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 
 });
+
+//Event listeners para los select de busqueda.
+marca.addEventListener('change', e => {
+  datosBusqueda.marca = e.target.value
+  filtrarAuto();
+});
+year.addEventListener('change', e => {
+  datosBusqueda.year = e.target.value
+});
+minimo.addEventListener('change', e => {
+  datosBusqueda.minimo = e.target.value
+});
+maximo.addEventListener('change', e => {
+  datosBusqueda.maximo = e.target.value
+});
+puertas.addEventListener('change', e => {
+  datosBusqueda.puertas = e.target.value
+});
+transmision.addEventListener('change', e => {
+  datosBusqueda.transmision = e.target.value
+});
+color.addEventListener('change', e => {
+  datosBusqueda.color = e.target.value
+});
+
+
 
 //Funciones.
 function mostrarAutos() {
@@ -40,3 +88,20 @@ function llenarSelect() {
     year.appendChild(option)
   }
 }
+
+//Filtrar en base a la busqueda.
+function filtrarAuto() {
+  
+  const resultado = autos.filter(filtrarMarca)
+  console.log(resultado);
+}
+
+function filtrarMarca(auto) {
+  const { marca, } = datosBusqueda;
+  if ( datosBusqueda.marca ) {
+    return auto.marca === datosBusqueda.marca;
+  }
+  return auto;
+}
+
+
